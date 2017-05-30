62d56ac86521742abd7762cdeb976ac3941a3a59
MatthewSickler <matthew.sickler@indyimaging.com>
2017-05-30 16:02:16 -0400

    configure angular cli, bootstrap the hybrid application together, load the ng-view in the base app.component, and update picture paths

--- angular-phonecat-hybrid/src/app/app.module.ts
+++ angular-phonecat-hybrid/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { UpgradeModule } from '@angular/upgrade/static';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    UpgradeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-phonecat-hybrid/src/main.ts
+++ angular-phonecat-hybrid/src/main.ts
@@ -1,5 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { UpgradeModule } from '@angular/upgrade/static';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 // Import these globally to bring in their @types
 import 'angular';
@@ -10,11 +17,9 @@ import 'angular-route';
 // And import our AngularJS module
 import './app-ajs';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule).then(platformRef => {
+  // Use the upgrade module to bootstrap the hybrid
+  const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
+  upgrade.bootstrap(document.documentElement, ['phonecatApp']);
+});
