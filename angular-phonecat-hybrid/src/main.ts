85835dc16cf271a2c246ed4db02e3e0fd1b9050f
MatthewSickler <matthew.sickler@indyimaging.com>
2017-05-30 15:23:09 -0400

    brought over phonecat source code, converted to typescript, created import chain and updated paths to be relative to the current file

--- angular-phonecat-hybrid/src/main.ts
+++ angular-phonecat-hybrid/src/main.ts
@@ -1,6 +1,15 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// Import these globally to bring in their @types
+import 'angular';
+import 'angular-animate';
+import 'angular-resource';
+import 'angular-route';
+
+// And import our AngularJS module
+import './app-ajs';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
